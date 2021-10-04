17be7bda5dc10f3314dfbbf0db6635347cacfd3a
Sipho Xaba <siphoxaba8@gmail.com>
2021-10-05 00:27:40 +0200

    Task 0.9 - Added commas

--- task0.9.js
+++ task0.9.js
@@ -18,16 +18,17 @@ function checkVowels(x){
 function printVowels(stringName){
 
     newString = stringName.toLowerCase();
-    var vowels = "";
-    var newVowels = "";
+    let vowels = "";
+    let newVowels = "";
+    let newWord = "";
     for(var i = 0; i <= newString.length; i++){
        
         vowels += checkVowels(newString[i]); 
     
 }
-var value=false;
-var newVowel=vowels[0];
-var x=0;
+let value=false;
+let newVowel=vowels[0];
+let x=0;
 for(var i = 0; i < vowels.length; i++){
     for(var j = 0; j < newVowel.length; j++){
     if(vowels[i]!=newVowel[j])
@@ -41,11 +42,20 @@ for(var i = 0; i < vowels.length; i++){
     }
     x=0;
 }
-    console.log("Vowels = " + newVowel);
+for( var i = 0; i < newVowel.length; i++){
+   if(newVowel.length == 1){
+      newWord = newVowel[i];
+   }
+    else if(i < newVowel.length){
+        newWord += newVowel[i] + ",";
+   }
 
+}
+console.log("Vowels = " + newWord);
 }
 
 printVowels("Umuzi");
 printVowels("books");
 printVowels("coding");
 printVowels("GENERAL");
+printVowels("bat");
